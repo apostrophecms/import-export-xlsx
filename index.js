f74a5cc380d671fe6850817f41b07418835fea91
Etienne Laurent <etldev@tuta.io>
2024-04-24 18:54:00 +0200

    registerFormats

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const xlsx = require('./lib/xlsx');
 
 module.exports = {
   improve: '@apostrophecms/import-export',
-  options: {
-    formats: {
-      xlsx
-    }
+  init(self) {
+    self.registerFormats({ xlsx });
   }
 };
